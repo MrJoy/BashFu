ea9453f0e2071ea49caaab7e169e187e44d7c4f8
Jon Frisby <jfrisby@mrjoy.com>
2013-04-07 23:19:30 -0700

    FIX: Clock didn't like hour/minute beginning with zero...

--- bin/clock.sh
+++ bin/clock.sh
@@ -4,7 +4,7 @@ CLOCK_FACES=( "🕛 🕧" "🕐 🕜" "🕑 🕝" "🕒 🕞" "🕓 🕟" "🕔 
 
 function rounded_time() {
   local RAW_HOUR=$(( ${1} + 0 ))
-  local RAW_MINUTE=$(( ${2} + 0 ))
+  local RAW_MINUTE=$(( ${2#0} + 0 ))
   local REMAINDER=$((RAW_MINUTE % 30))
   if [[ $REMAINDER -gt 15 ]]; then
     REMAINDER=-$((30-REMAINDER))
@@ -12,19 +12,16 @@ function rounded_time() {
   local COERCED_HOUR="$RAW_HOUR"
   local COERCED_MINUTE="$((RAW_MINUTE - REMAINDER))"
   if [[ $COERCED_MINUTE -ge 60 ]]; then
-    COERCED_HOUR=$(( (RAW_HOUR + 1) % 24 ))
+    COERCED_HOUR=$(( (RAW_HOUR + 1) % 12 ))
     COERCED_MINUTE='0'
   fi
   COERCED_MINUTE=$((COERCED_MINUTE / 30))
-  if [[ $COERCED_HOUR -ge 12 ]]; then
-    COERCED_HOUR=$((COERCED_HOUR - 12))
-  fi
   echo $COERCED_HOUR $COERCED_MINUTE
 }
 
 function show_clock() {
-  local RAW_TIME=( $(date +"%H %M") )
-  local INDEXES=( $(rounded_time $((RAW_TIME[0] + 0)) $((RAW_TIME[1] + 0))) )
+  local RAW_TIME=( $(date +"%l %M") )
+  local INDEXES=( $(rounded_time ${RAW_TIME[0]} ${RAW_TIME[1]}) )
   local FACES_FOR_HOUR=( ${CLOCK_FACES[INDEXES[0]]} )
   echo "${FACES_FOR_HOUR[INDEXES[1]]} "
 }
